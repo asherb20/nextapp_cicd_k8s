658aecc63353c8b25f75a9d58fae5064074d2410
Asher Best <ashermcbest@gmail.com>
2026-05-21 20:28:25 -0500

    testing pre-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import Image from 'next/image';
 
-// Fake API key for testing pre-commit hook
-const FAKE_API_KEY = 'sk_test_51234567890abcdefghijklmnop';
-
 export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
